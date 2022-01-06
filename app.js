e5f928822a785be6d46315b82419da7b00a6e929
Emre BULAK <emrbrnn@gmail.com>
2022-01-06 17:36:31 +0300

    Mongoose created and post added.

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
 
+const Photo = require('./models/Post');
+const Post = require('./models/Post');
+
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/clean-blog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
 //Template Engine
 app.set('view engine', 'ejs');
 //Template Engine
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+  const post = await Post.find({});
+  res.render('index', {
+    post,
   });
+});
 
   app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/add', (req, res) => {
-  res.render('add');
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.post('/post', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.get('*', (req, res) => {
